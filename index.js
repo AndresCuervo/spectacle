bbeed8dce7c6c6c73103376aecfe7bc140b8015b
Andres Cuervo <andrescuervor@gmail.com>
2018-01-09 00:00:27 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,129 @@
+tracking.ColorTracker.registerColor('blue', function(r, g, b) {
+    if (r < 50 && g > 50 && b < 200) {
+        return true;
+    }
+    return false;
+});
+
+let colors = ['magenta', 'blue']
+let tracker = new tracking.ColorTracker(colors)
+
+let data = {}
+let gui = new dat.GUI();
+
+//let canvas = document.getElementById('canvas')
+// let context = canvas.getContext('2d')
+
+let video = document.querySelector('#myVideo')
+
+tracking.track('#myVideo', tracker, { camera: true })
+
+let state, pg, canvas, cString
+
+// Resize the canvas to the video size when stream starts
+video.addEventListener('loadeddata', e => {
+    console.log(video.getBoundingClientRect())
+    canvasToVideoSize()
+})
+
+let textDimensions = {
+    x: 30,
+    y: 30,
+    width: 300,
+    height: 60
+}
+
+function canvasToVideoSize() {
+    var x = (windowWidth - width) / 2;
+    var y = (windowHeight - height) / 2;
+    // canvas.position(x, y);
+
+    let dimensions = video.getBoundingClientRect()
+    canvas.size(dimensions.width, dimensions.height)
+}
+
+function setup() {
+    // canvas = createCanvas(window.screen.width, window.screen.height)
+    let dimensions = video.getBoundingClientRect()
+    canvas = createCanvas(dimensions.width, dimensions.height)
+
+    // Move the canvas so it's inside our <div id="sketch-holder">.
+    canvas.parent('sketch-holder')
+
+    background(255, 0, 200)
+
+    state = {
+        r: 0,
+        sw: 3
+    }
+
+
+    gui.add(state, "sw", 0, 10);
+
+    pg = createGraphics(100, 100)
+    pg.position.x -= 9999
+
+    cString = colors.join(' or ')
+
+
+    // pg.clear();
+    // pg.noFill();
+    // pg.stroke(255);
+    // ellipse(40, 40, 40, 40)
+    canvasToVideoSize()
+}
+
+function draw() {
+    clear()
+
+    // fill(255);
+    // noStroke();
+    // ellipse(mouseX, mouseY, 60, 60);
+
+    pg.background(51);
+    pg.noFill();
+    pg.stroke(255);
+    // pg.ellipse(mouseX-pg.width/2, mouseY-pg.height/2, 60, 60);
+    let r = Math.sin(performance.now() / 1000)
+    pg.ellipse(pg.width / 4, pg.height / 4, r * pg.width / 2, r * pg.height /2);
+    // image(pg, 0, 0);
+
+    //Draw the offscreen buffer to the screen with image()
+    // image(pg, state.r * 30, 75);
+
+    if (data.length) {
+        textFont('Helvetica', 11)
+
+        data.forEach(data => {
+            noFill()
+            strokeWeight(state.sw)
+            stroke('magenta')
+            rect(data.x, data.y, data.width, data.height)
+
+            fill('white')
+            noStroke()
+            text(`x: ${data.x} px`, data.x + data.width + 5, data.y + 11, 11, 70)
+            let imageX = data.x + data.width / 2 - pg.width/ 2
+            let imageY = data.y + data.height/ 2 - pg.height/ 2
+            image(pg, imageX, imageY);
+        })
+    } else {
+        textFont('Helvetica', 20)
+
+        fill('white')
+        noStroke()
+        rect(textDimensions.x, textDimensions.y, textDimensions.width, textDimensions.height)
+
+        fill('black')
+        text(`No ${cString} in frame, womp womp.`, textDimensions.x, textDimensions.y, textDimensions.width, textDimensions.height)
+    }
+}
+
+
+tracker.on('track', function(e) {
+    data = e.data
+})
+
+function windowResized() {
+    canvasToVideoSize();
+}
